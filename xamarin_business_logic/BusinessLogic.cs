e3ba7ed7ef8b84289f28a89838579e5a87c1919d
Dennis Kreuzer <dk098@hdm-stuttgart.de>
2020-02-27 01:38:05 +0100

    initial commit

---
+++ xamarin_business_logic/BusinessLogic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace xamarin_business_logic
+{
+    public class BusinessLogic
+    {
+        public static string getImage(bool toggle)
+        {
+            if (toggle)
+            {
+                return "lightOn.png";
+            }
+            else
+            {
+                return "lightOff.png";
+            }
+        }
+
+        public static string getRoom()
+        {
+            return "Living Room";
+        }
+    }
+}
